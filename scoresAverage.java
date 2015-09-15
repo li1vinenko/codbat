47fe67c46ebdd18fd4884d36e2d544621d8e4338
li1vinenko <litvinenko_zhek@mail.ru>
2015-09-16 01:30:50 +0300

    Create scoresAverage.java

---
+++ scoresAverage.java
@@ -0,0 +1,22 @@
+public int scoresAverage(int[] scores) {
+
+  int begOfArr = 0;
+  int halfOfArr = scores.length/2;
+  int endOfArr = scores.length;
+  int aver1 = average(scores, begOfArr, halfOfArr);
+  int aver2 = average (scores, halfOfArr, endOfArr);
+
+  if (aver1 - aver2 > 0) {
+      return aver1;
+      } else {
+      return aver2;
+      }
+}
+
+public int average(int[] scores, int start, int end) {
+  int sum = 0;
+  for (int i = start; i < end; i++) {
+      sum = sum + scores[i];
+      } 
+  return sum/(scores.length/2);
+}
